55140295087d6ba83d34a6ec63113ec3e7607ca8
Renato Oliveira <renato.dev.oliveira@gmail.com>
2024-10-28 17:40:44 -0300

    [2015]: Day 03 - Perfectly Spherical Houses in a Vacuum (Part 2)

--- 2015/03/main.go
+++ 2015/03/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	input := utils.ReadInput("input.txt")
 	fmt.Println("Answer 1:", first(input))
+	fmt.Println("Answer 2:", second(input))
 }
 
 var directions = map[rune][2]int{
@@ -26,6 +27,25 @@ func first(input string) int {
 		move = directions[r]
 		santa = [2]int{santa[0] + move[0], santa[1] + move[1]}
 		houses[santa] = true
+	}
+
+	return len(houses)
+}
+
+func second(input string) int {
+	houses := make(map[[2]int]bool)
+	var move, santa, robot [2]int
+	houses[santa] = true
+
+	for i, r := range input {
+		move = directions[r]
+		if i%2 == 0 {
+			santa = [2]int{santa[0] + move[0], santa[1] + move[1]}
+			houses[santa] = true
+		} else {
+			robot = [2]int{robot[0] + move[0], robot[1] + move[1]}
+			houses[robot] = true
+		}
 	}
 
 	return len(houses)
