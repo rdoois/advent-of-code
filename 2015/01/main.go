536625642c4b5f6bd01bef155875d198f413554e
Renato Oliveira <renato.dev.oliveira@gmail.com>
2024-10-28 14:06:52 -0300

    [2015]: Day 01 - Not Quite Lisp (Part 2)

--- 2015/01/main.go
+++ 2015/01/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	input := utils.ReadInput("input.txt")
 	fmt.Println("Answer 1:", first(input))
+	fmt.Println("Answer 2:", second(input))
 }
 
 func first(input string) int {
@@ -23,4 +24,22 @@ func first(input string) int {
 	}
 
 	return floor
+}
+
+func second(input string) int {
+	var floor int
+
+	for i, r := range input {
+		if r == '(' {
+			floor += 1
+		} else if r == ')' {
+			floor -= 1
+		}
+
+		if floor == -1 {
+			return i + 1
+		}
+	}
+
+	return 0
 }
