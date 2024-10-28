d9890f29e690a94f860d26367d673a55242991bf
Renato Oliveira <renato.dev.oliveira@gmail.com>
2024-10-28 14:42:21 -0300

    [2015]: Day 02 - I Was Told There Would Be No Math (Part 2)

--- 2015/02/main.go
+++ 2015/02/main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	input := utils.ReadInput("input.txt")
 	fmt.Println("Answer 1:", first(input))
+	fmt.Println("Answer 2:", second(input))
 }
 
 func first(input string) int {
@@ -28,4 +29,28 @@ func first(input string) int {
 	}
 
 	return paper
+}
+
+func second(input string) int {
+	var ribbon int
+	var lines = strings.Split(input, "\n")
+
+	for _, line := range lines {
+		var lenght, width, heigth int
+		fmt.Sscanf(line, "%dx%dx%d", &lenght, &width, &heigth)
+
+		var min1, min2 int
+		min1 = min(lenght, width)
+
+		if min1 == lenght {
+			min2 = min(width, heigth)
+		} else {
+			min2 = min(lenght, heigth)
+		}
+
+		ribbon += (2 * min1) + (2 * min2) + (lenght * width * heigth)
+
+	}
+
+	return ribbon
 }
